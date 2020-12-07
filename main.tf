cdce20ed74baf1343d7f7aeee827f33e9af7e4ba
Veróczki <laszlo.veroczki@thermofisher.com>
2020-12-07 12:11:57 +0100

    s3 bucket setup

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_s3_bucket" "terraform_state" {
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AE256"
+        sse_algorithm = "AES256"
       }
     }
   }
@@ -28,4 +28,25 @@ resource "aws_dynamodb_table" "terraform_locks" {
     name = "LockID"
     type = "S"
   }
+}
+
+terraform {
+  backend "s3"{
+    bucket  = "terraform-up-and-running-state-lacie"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+
+    dynamodb_table = "terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
+
+output "s3_bucket_arn" {
+  value = aws_s3_bucket.terraform_state.arn
+  description = "The ARN of the S3 bucket"
+}
+
+output "dynamodb_table_name" {
+  value = aws_dynamodb_table.terraform_locks.name
+  description = "The name of the DynamoDB table"
 }
