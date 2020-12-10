64da78f802346b3484fd9c120a9d6d437c910695
Laci <werolac@gmail.com>
2020-12-10 10:57:28 +0100

    refacto alb

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -15,53 +15,6 @@ data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_lb" "example" {
-  name               = var.cluster_name
-  load_balancer_type = "application"
-  subnets            = data.aws_subnet_ids.default.ids
-  security_groups    = [aws_security_group.alb.id]
-}
-
-resource "aws_lb_listener" "http" {
-  load_balancer_arn = aws_lb.example.arn
-  port              = local.http_port
-  protocol          = "HTTP"
-  default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "404: page not found"
-      status_code  = 404
-    }
-  }
-}
-
-resource "aws_security_group" "alb" {
-  name = "${var.cluster_name}-alb"
-  ingress = [{
-    cidr_blocks      = local.all_ips
-    description      = "asd"
-    from_port        = local.http_port
-    ipv6_cidr_blocks = local.all_ips
-    prefix_list_ids  = []
-    protocol         = local.tcp_protocol
-    security_groups  = ["sg-93ba2be2"]
-    self             = false
-    to_port          = local.http_port
-  }]
-  egress = [{
-    cidr_blocks      = local.all_ips
-    description      = "asd"
-    from_port        = local.any_port
-    ipv6_cidr_blocks = local.all_ips
-    prefix_list_ids  = []
-    protocol         = local.any_protocol
-    security_groups  = ["sg-93ba2be2"]
-    self             = false
-    to_port          = local.any_port
-  }]
-}
-
 resource "aws_lb_target_group" "asg" {
   name     = var.cluster_name
   port     = var.server_port
