9b885ecab52891b7b5fa2e2c1972682063e5f24d
Laci <werolac@gmail.com>
2020-12-10 10:49:42 +0100

    started refacto asg

--- global/s3/main.tf
+++ global/s3/main.tf
@@ -31,13 +31,13 @@ resource "aws_dynamodb_table" "terraform_locks" {
 }
 
 terraform {
-  backend "s3"{
-    bucket  = "terraform-up-and-running-state-lacie"
-    key = "stage/services/webserver-cluster/terraform.tfstate"
+  backend "s3" {
+    bucket = "terraform-up-and-running-state-lacie"
+    key    = "stage/services/webserver-cluster/terraform.tfstate"
     region = "us-east-2"
 
     dynamodb_table = "terraform-up-and-running-locks"
-    encrypt = true
+    encrypt        = true
   }
 }
 
